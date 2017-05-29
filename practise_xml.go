79f860cf730b61f9d800ad8644f32ac555edcd35
durban.zhang <durban.zhang@gmail.com>
2017-05-29 18:38:46 +0800

    add build xml

--- practise_xml.go
+++ practise_xml.go
@@ -7,17 +7,43 @@ import (
 	"os"
 )
 
-type servers struct {
+type Servers struct {
 	XMLName     xml.Name `xml:"servers"`
 	Version     string   `xml:"version,attr"`
-	Server      []server `xml:"server"`
+	Server      []Server `xml:"server"`
 	Description string   `xml:",innerxml"`
 }
 
-type server struct {
+type Server struct {
 	XMLName    xml.Name `xml:"server"`
 	ServerName string   `xml:"serverName"`
 	ServerIP   string   `xml:"serverIP"`
+}
+
+type XMLServers struct {
+	XMLName xml.Name    `xml:"servers"`
+	Version string      `xml:"version,attr"`
+	Server  []XMLServer `xml:"server"`
+}
+
+type XMLServer struct {
+	ServerName string `xml:"serverName"`
+	ServerIP   string `xml:"serverIP"`
+}
+
+func BuildXml() {
+	v := &XMLServers{Version: "1"}
+	v.Server = append(v.Server, XMLServer{"Shanghai_VPN", "127.0.0.1"})
+	v.Server = append(v.Server, XMLServer{"Beijing_VPN", "127.0.0.2"})
+
+	output, err := xml.MarshalIndent(v, " ", "  ")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	os.Stdout.Write([]byte(xml.Header))
+	os.Stdout.Write(output)
 }
 
 func main() {
@@ -35,7 +61,7 @@ func main() {
 		return
 	}
 
-	v := servers{}
+	v := Servers{}
 	err = xml.Unmarshal(data, &v)
 	if err != nil {
 		fmt.Printf("error: %v", err)
@@ -43,4 +69,6 @@ func main() {
 	}
 
 	fmt.Println(v)
+
+	BuildXml()
 }
